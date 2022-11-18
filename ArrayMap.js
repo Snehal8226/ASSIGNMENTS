1c74fd5ede7a0f87134dcd7ec2eb91cfdb3a8e99
Snehal <117708032+Snehal8226@users.noreply.github.com>
2022-11-18 13:48:59 +0530

    update

--- ArrayMap.js
+++ ArrayMap.js
@@ -1,1 +1,12 @@
-set
+
+//Array Map  (Map always in key n value pair key can be any datatype)
+
+//1.set()method
+
+    const vehicals = new Map();
+
+  // Set Map Values
+     vehicals.set("Jupiter", 700000);
+     vehicals.set("Java42", 2500000);
+
+     document.write(vehicals.get("Java42"));
