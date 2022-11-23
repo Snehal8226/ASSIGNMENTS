a838d98df862959f5b63bed1b674f7b345d9fec5
Snehal <117708032+Snehal8226@users.noreply.github.com>
2022-11-23 12:08:17 +0530

    trial changes

--- ArrayMap.js
+++ ArrayMap.js
@@ -3,10 +3,86 @@
 
 //1.set()method
 
-    const vehicals = new Map();
+    /*const vehicals = new Map();
 
   // Set Map Values
      vehicals.set("Jupiter", 700000);
      vehicals.set("Java42", 2500000);
 
      document.write(vehicals.get("Java42"));
+
+     //2.The get() method
+
+     const vehicals = new Map();
+     vehicals.set("Jupiter", 700000);
+     vehicals.set("Java42", 2500000);
+
+     document.write(vehicals.get("Jupiter"));
+
+     //3. size method
+     const vehicals = new Map();
+     vehicals.set("Jupiter", 700000);
+     vehicals.set("Java42", 2500000);
+
+     document.write(vehicals.size);
+
+     //3. delete method
+     const vehicals = new Map();
+     vehicals.set("Jupiter", 700000);
+     vehicals.set("Java42", 2500000);
+
+     vehicals.delete("Jupiter");
+     document.write(vehicals.size);
+
+     //3. has() method it returns true if value present in map
+     const vehicals = new Map();
+     vehicals.set("Jupiter", 700000);
+     vehicals.set("Java42", 2500000);
+
+     document.write(vehicals.has("Pulser"));*/
+
+
+     //function processArray(...str) {
+        document.write("Array practice print num 1 to 10", str);
+        var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        document.write(numbers);
+     //document.getElementById("placeholder").innerHTML = numbers;
+    for (let index = 0; index < numbers.length; index++) {
+      const element = numbers[index];
+      document.write(element);
+    }
+  //const names = ["Punam", "Sunil", "Snehal", "Pranav", "Prerana"];
+  //names.forEach((name, index) => {
+    // console.log("Element - ", name, " Index", index);
+    // console.log("------ name.startsWith('Pr') ---", name.startsWith("Pr"));
+    // console.log("------ name.endsWith('l') ---- ", name.endsWith("l"));
+    // console.log("------ name.includes('u') ---- ", name.includes("u"));
+  //});
+  //const indexOfSunil = names.indexOf("Sunil");
+  //   console.log("Index of Sunil is - ", indexOfSunil);
+  //
+  /*const match = names.find((name, index) => {
+    const starts = name.startsWith("S");
+    const ends = name.endsWith("l");
+    return starts && ends;
+  });
+  //   console.log("Match = ", match);
+  const matchAll = names.filter((name, index) => {
+    const starts = name.startsWith("S");
+    const ends = name.endsWith("l");
+    return starts && ends;
+  });
+  //   console.log("Match All = ", matchAll);
+  
+processArray("ABC");
+processArray("ABC", "PQR");
+processArray("ABC", "PQR", "XYZ");
+processArray("ABC", "PQR", "XYZ", "12312312sda");*/
+
+
+
+
+
+
+
+
