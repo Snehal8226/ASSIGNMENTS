2b229fecefe1b2854f5d510dca981a20f0cc3270
Snehal <117708032+Snehal8226@users.noreply.github.com>
2022-12-06 14:48:11 +0530

    node practice add

---
+++ nodepractise/index.js
@@ -0,0 +1,24 @@
+
+// var momemt=require('moment')
+// console.log(moment().format());
+
+console.log('Hiiiiiiiiii.!!!');
+
+
+
+var moment = require('moment'); // require
+//console.log(moment().format());
+
+
+//import moment from 'moment';
+//console.log(moment().format());
+//console.log(moment().millisecond());
+console.log(moment().minute());
+console.log(moment().date()); // Number
+//console.log(moment().year(year).month(month).date(day));
+console.log(moment().day()); // Number
+console.log(moment().day(-7)); // last Sunday (0 - 7)
+console.log(moment().day(0));// this Sunday (0)
+console.log(moment().day(7));// next Sunday (0 + 7)
+console.log(moment().day(10)); // next Wednesday (3 + 7)
+console.log(moment().day(24)); // 3 Wednesdays from now (3 + 7 + 7 + 7)
