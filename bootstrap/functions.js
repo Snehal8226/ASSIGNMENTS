9c2201c4a3bc0764c964a5f87f647d2d6a379434
Snehal <117708032+Snehal8226@users.noreply.github.com>
2022-12-06 15:34:20 +0530

    node add

--- bootstrap/functions.js
+++ bootstrap/functions.js
@@ -1,5 +1,52 @@
-function submit()
-{
-    alert("Profile created.........!!!!!");
 
-} 
+function submit(){
+
+         
+         var firstname = document.getElementById('inputtext').value
+         document.write("firstname");
+         
+          var lastname = document.getElementById('inputtext').value
+         document.write("lastname");
+
+         var emailaddress = document.getElementById('exampleFormControlInput1').value
+         document.write("emailaddress");
+
+         var  address = document.getElementById('inputAddress').value
+         document.write("address");
+
+         var education  = document.getElementById('inputeducation').value
+         document.write("education");
+
+         var gender  = document.getElementById('inputgender').value
+         document.write("gender");
+         var male  = document.getElementById('flexRadioDefault1').value
+         document.write("male");
+         var female  = document.getElementById('flexRadioDefault1').value
+         document.write("female");
+         var other  = document.getElementById('flexRadioDefault1').value
+         document.write("other");
+
+         var city  = document.getElementById("inputCity").value;
+         document.write("city");
+
+         var state  = document.getElementById('inputState').value
+         document.write("state");
+
+         const dynamicvalue={
+                "name":"firstname",
+                "lname":"lastname",
+                "emailaddress":"emailaddress",
+                "address":"address",
+                "education":"education",
+                "gender":"gender",
+                "male":"male",
+                "female":"female",
+                "other":"other",
+                "city":"city",
+                "state":"state"
+
+         }
+         cosole.log("dynamic value",dynamicvalue);
+         if(confirm(JSON.stringify(dynamicvalue)))
+        }
+        
