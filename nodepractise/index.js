f48cfe78b1baefc4b22fde11be7f776748417181
Snehal <117708032+Snehal8226@users.noreply.github.com>
2022-12-08 16:20:48 +0530

    add new node

--- nodepractise/index.js
+++ nodepractise/index.js
@@ -25,3 +25,11 @@ console.log(moment().day(7));// next Sunday (0 + 7)
 console.log(moment().day(10)); // next Wednesday (3 + 7)
 console.log(moment().day(24)); // 3 Wednesdays from now (3 + 7 + 7 + 7)
 console.log(moment().day("Sunday"));
+//this show todays day in number
+console.log(moment().weekday());
+console.log(moment().weekday());
+// when Monday is the first day of the week
+//console.log(moment().weekday(0));
+//console.log(moment().weekday(-7)); // last Monday
+ //console.log(moment().weekday(7)); // next Monday
+ console.log(moment().weekday(-7));
