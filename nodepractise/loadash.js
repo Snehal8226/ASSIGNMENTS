3d3d9c9d39cfffcf5ef978b2638e56146fb9bc5f
Snehal <117708032+Snehal8226@users.noreply.github.com>
2022-12-14 18:41:51 +0530

    todos

---
+++ nodepractise/loadash.js
@@ -0,0 +1,31 @@
+
+
+const axios = require("axios");
+//const lodash =require("lodash");
+
+const getTodos =() => {
+        //implement
+        //GET
+         //const url = "https://jsonplaceholder.typicode.com/todos/4";
+         const url = "https://jsonplaceholder.typicode.com/user";
+         console.log("Making api");
+             return axios
+             .get(url)
+             .then((response) => {
+                console.log(response.data);
+             })
+
+            .catch((error) => {
+               console.log("reciving api");
+             console.error(error);
+      });
+
+   }; 
+      console.log("Before Making API call");
+      getTodos();
+      console.log("After Making API call");
+
+
+    
+      
+
